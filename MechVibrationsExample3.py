fe05a3dd8b521f1518f29e413a80002d16c04abb
fsb31 <fsb31@cam.ac.uk>
2026-05-14 19:10:41 +0100

    mech vibrations ex3

--- MechVibrationsExample3.py
+++ MechVibrationsExample3.py
@@ -1,12 +1,34 @@
 import numpy as np
 
 
-m = np.array([[1, 0], [0, 2]])
+#Q3
+'''
+m = np.matrix([[1, 0], 
+              [0, 2]])
 
-k = np.array([[2, -1], [-1, 3]])
+k = np.matrix([[2, -1], 
+              [-1, 3]])
+'''
+
+#Q5
+m1 = 1
+m2=3
+k1=9
+
+m = np.matrix([[m1, 0, 0], 
+              [0, m2, 0],
+              [0, 0, m1]])
+
+k = k1 * np.matrix([[1, -1, 0], 
+              [-1, 2, -1],
+              [0, -1, 1]])
+
 
 #Finds eigenvalues and vectors of M^-1 * K
 freq, modes = np.linalg.eig(np.linalg.inv(m) * k)
 
-print(freq)
-print(modes)
+#sometimes if a freq = 0, it is stored as -0, so sqrt throws error... just ignore
+
+for i in range(len(freq)):
+    print(f"Natural Freq Squared: {round(freq[i],3)}, Natural Freq: {round(np.sqrt(freq[i]),3)}")
+    print(f"Mode Shape: {np.round(np.array((modes[:,i]/modes[0,i])).flatten().squeeze(),3)}") #converts from matrix to 2d array and removes extra dimensions
